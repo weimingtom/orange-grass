46d20802075ad120064c5f5457ac58e31b758c11
viacheslav.bogdanov <viacheslav.bogdanov@419852ea-0cf1-11df-8eb8-7520fd0e61c4>
2011-01-16 15:56:51 +0000

    [og] fixed frustum check

--- engine/src/win32/ogactormanager.cpp
+++ engine/src/win32/ogactormanager.cpp
@@ -239,13 +239,23 @@ void COGActorManager::Update (unsigned long _ElapsedTime)
 		case OG_ACTOR_LANDBOT:
 		case OG_ACTOR_BONUS:
 			{
-				float fObjectZ = pActor->GetPhysicalObject()->GetPosition().z;
-				if (fObjectZ <= fCameraZ)
+				//float fObjectZ = pActor->GetPhysicalObject()->GetPosition().z;
+				//if (fObjectZ <= fCameraZ)
+				//{
+				//	if ((fCameraZ - fObjectZ) < m_fViewDistance)
+				//	{
+				//		pActor->Activate(true);
+				//	}
+				//}
+				//else
+				//{
+				//	if (pActor->IsActive())
+				//		pActor->Activate(false);
+				//}
+				if (pCamera->GetFrustum().CheckObb(pActor->GetSgNode()->GetOBB()))
 				{
-					if ((fCameraZ - fObjectZ) < m_fViewDistance)
-					{
+					if (!pActor->IsActive())
 						pActor->Activate(true);
-					}
 				}
 				else
 				{
