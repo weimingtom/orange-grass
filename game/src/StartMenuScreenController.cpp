2cf06024b9d616558e85f4a47332798599d30f6d
Viacheslav.Bogdanov <Viacheslav.Bogdanov@419852ea-0cf1-11df-8eb8-7520fd0e61c4>
2011-03-24 11:49:19 +0000

    updating version

--- game/src/StartMenuScreenController.cpp
+++ game/src/StartMenuScreenController.cpp
@@ -133,7 +133,7 @@ void CStartMenuScreenController::RenderScene ()
 #endif
 	m_pRenderer->FinishRenderMode();
 	m_pRenderer->StartRenderMode(OG_RENDERMODE_TEXT);
-    m_pRenderer->DisplayString(m_DemoLabelPos, 0.3f, 0x7FFFFFFF, "Demo version: %d.%d", 0, 18);
+    m_pRenderer->DisplayString(m_DemoLabelPos, 0.3f, 0x7FFFFFFF, "Demo version: %d.%d", 0, 19);
 	m_pRenderer->FinishRenderMode();
 	m_pRenderer->Reset();
 }
