3a4f96d1a62801ac9001480f689cccfa16af40ef
viacheslav.bogdanov <viacheslav.bogdanov@419852ea-0cf1-11df-8eb8-7520fd0e61c4>
2012-01-01 17:59:02 +0000

    adding zlib and unzip makefiles

---
+++ engine/src/unzip/Android.mk
@@ -0,0 +1,6 @@
+LOCAL_PATH := $(call my-dir)
+
+LOCAL_SRC_FILES := \
+                    ioapi.c \
+                    unzip.c \
+                    zip.c \
---
+++ engine/src/zlib/Android.mk
@@ -0,0 +1,15 @@
+LOCAL_PATH := $(call my-dir)
+
+LOCAL_SRC_FILES := \
+                    adler32.c \
+                    compress.c \
+                    crc32.c \
+                    deflate.c \
+                    gzio.c \
+                    infback.c \
+                    inffast.c \
+                    inflate.c \
+                    inftrees.c \
+                    trees.c \
+                    uncompr.c \
+                    zutil.c \
