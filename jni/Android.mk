009f43f51d3adcef07860e55d2ee210e0a319f9c
viacheslav.bogdanov <viacheslav.bogdanov@419852ea-0cf1-11df-8eb8-7520fd0e61c4>
2012-01-01 19:34:44 +0000

    adding android makefiles

--- jni/Android.mk
+++ jni/Android.mk
@@ -4,9 +4,13 @@ ENGINEDIR := $(LOCAL_PATH)/engine
 include $(CLEAR_VARS)
 include $(ENGINEDIR)/src/zlib/Android.mk
 include $(ENGINEDIR)/src/unzip/Android.mk
+include $(ENGINEDIR)/src/tinyxml/Android.mk
+include $(ENGINEDIR)/src/pvr/Android.mk
+include $(ENGINEDIR)/src/Android.mk
 
-LOCAL_MODULE    := liborangegrass
-LOCAL_CFLAGS    := -Werror
-LOCAL_LDLIBS    := -llog -lGLESv2
+LOCAL_MODULE     := liborangegrass
+LOCAL_CFLAGS     := -Werror
+LOCAL_C_INCLUDES += $(ENGINEDIR)/include
+LOCAL_LDLIBS     := -llog -lGLESv2
 
 include $(BUILD_SHARED_LIBRARY)
