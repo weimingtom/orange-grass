ae8fa6ae33538c24dc7a4f876c628fb7883d8a7c
viacheslav.bogdanov <viacheslav.bogdanov@419852ea-0cf1-11df-8eb8-7520fd0e61c4>
2012-01-03 18:57:18 +0000

    adding android make files

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,5 +1,6 @@
 LOCAL_PATH := $(realpath $(call my-dir)/..)
 ENGINEDIR := $(LOCAL_PATH)/engine
+GAMEDIR := $(LOCAL_PATH)/skycrasher
 
 include $(CLEAR_VARS)
 include $(ENGINEDIR)/src/zlib/Android.mk
@@ -7,10 +8,12 @@ include $(ENGINEDIR)/src/unzip/Android.mk
 include $(ENGINEDIR)/src/tinyxml/Android.mk
 include $(ENGINEDIR)/src/pvr/Android.mk
 include $(ENGINEDIR)/src/Android.mk
+include $(GAMEDIR)/game/src/Android.mk
 
 LOCAL_MODULE     := liborangegrass
 LOCAL_CFLAGS     := -Werror
-LOCAL_C_INCLUDES += $(ENGINEDIR)/include
+LOCAL_C_INCLUDES += $(ENGINEDIR)/include \
+                    $(GAMEDIR)/game/include
 LOCAL_LDLIBS     := -llog -lGLESv2
 
 include $(BUILD_SHARED_LIBRARY)
